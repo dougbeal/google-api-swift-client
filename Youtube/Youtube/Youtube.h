3fcc61d51adb51adb5c36b52b5264929d9a47186
Douglas Beal <git@dougbeal.com>
2015-03-23 14:44:30 -0700

    Youtube playground and project

---
+++ Youtube/Youtube/Youtube.h
@@ -0,0 +1,19 @@
+//
+//  Youtube.h
+//  Youtube
+//
+//  Created by Douglas Beal on 3/23/15.
+//  Copyright (c) 2015 Murder Of Crows Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Youtube.
+FOUNDATION_EXPORT double YoutubeVersionNumber;
+
+//! Project version string for Youtube.
+FOUNDATION_EXPORT const unsigned char YoutubeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Youtube/PublicHeader.h>
+
+
